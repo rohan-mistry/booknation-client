6bffe2c3546b5daf4bcd15d6308f18a629ecf6c3
rohan-mistry <mistryrohan13@gmail.com>
2022-03-17 11:39:19 +0530

    implemented add to cart, cart checkout, book details page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,90 @@
-import Protected from "./components/AuthRoutes/Protected";
-import SignInRoute from "./components/AuthRoutes/SignInRoute";
+import React,{ useState, useEffect} from 'react';
 import Login from "./pages/auth/Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Error from "./pages/error/Error";
 import Home from "./pages/home/Home";
 import SignUp from "./pages/auth/SignUp";
 import Navbar from "./components/Navbar";
+import ProductDetail from './pages/product/ProductDetail';
+import Cart from './pages/cart/Cart';
+import Checkout from './pages/checkout/Checkout';
+
 
 function App() {
+  const [cart, setCart] = useState([])
+  
+  const addItem = (item) => {
+    let itemExists = cart.some(elem => elem.item.id==item.id);
+    if(itemExists) {
+      console.log("item already exists");
+      return;
+    }
+    let items = [...cart,{item,quantity: 1}];
+    setCart(items);
+    let cartString = JSON.stringify(items);
+    localStorage.setItem('cart',cartString);
+  }
+
+  const updateItem = (itemID, quantity) => {
+    let cartCopy = [...cart]
+
+    let existentItem = cartCopy.find(elem => elem.item.id == itemID);
+    
+    if (!existentItem) return
+    
+    existentItem.quantity += quantity;
+
+    if (existentItem.quantity <= 0) {
+      cartCopy = cartCopy.filter(elem => elem.item.id != itemID)
+    }
+    
+    setCart(cartCopy);
+    
+    let cartString = JSON.stringify(cartCopy);
+    localStorage.setItem('cart', cartString);
+  }
+
+  const removeItem = (itemID) => {
+    let items = [...cart]
+  
+    items = items.filter(elem => elem.item.id != itemID);
+    
+    setCart(items);
+    
+    let cartString = JSON.stringify(items)
+    localStorage.setItem('cart', cartString)
+  }
+
+  const checkItem = (itemID) => {
+    let itemExists = cart.some(elem => elem.item.id == itemID);
+    return itemExists;
+  }
+  
+  useEffect(() => {
+    let localCart = localStorage.getItem("cart");
+    localCart = JSON.parse(localCart);
+    if (localCart) setCart(localCart)
+    console.log("mycart : ", cart)
+  }, []);
+
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar cartLength={cart.length}/>
       <Router>
         <Routes>
-          {/* <SignInRoute exact path="/signin" component={<Login/>}/> */}
           <Route path="/" element={<Home/>}/>
           <Route path="/signin" element={<Login/>}/>
           <Route path="/signup" element={<SignUp/>}/>
+          <Route path="/book/:bookID" element={<ProductDetail 
+            addCartItem={addItem}
+            checkItem={checkItem}
+          />}/>
+          <Route path="/cart" element={<Cart
+              cart={cart} 
+              updateItem={updateItem}
+              removeItem={removeItem}
+          />}/>
+          <Route path="/checkout" element={<Checkout cart={cart}/>}/>
           <Route path="*" element={<Error/>}/>
         </Routes>
       </Router>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,45 @@
+import { Container, Grid } from '@mui/material'
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import ProductCard from '../../components/ProductCard'
+const dummyData = [
+  {
+    coverPhoto: "https://images-na.ssl-images-amazon.com/images/I/61Iz2yy2CKL.jpg",
+    title:"The Monk who sold his ferrari",
+    description:"This inspiring tale provides a step-by-step approach to living with greater courage, balance, abundance, and joy. A wonderfully crafted fable, The Monk Who Sold His Ferrari tells the extraordinary story of Julian Mantle, a lawyer forced to confront the spiritual crisis of his out-of-balance life. On a life-changing odyssey to an ancient culture, he discovers powerful, wise, and practical lessons that teach us to : Develop Joyful Thoughts, Follow Our Life's Mission and Calling, Cultivate Self-Discipline and Act Courageously, Value Time as Our Most Important Commodity, Nourish Our Relationships, and Live Fully, One Day at a Time. ",
+    price: 460,
+    author: 'Robin Sharma',
+    id: 1
+  },
+  {
+    coverPhoto: "https://images-na.ssl-images-amazon.com/images/I/61tMpA6eyVL.jpg",
+    title:"Greatness Guide",
+    description:"This inspiring tale provides a step-by-step approach to living with greater courage, balance, abundance, and joy. A wonderfully crafted fable, The Monk Who Sold His Ferrari tells the extraordinary story of Julian Mantle, a lawyer forced to confront the spiritual crisis of his out-of-balance life. On a life-changing odyssey to an ancient culture, he discovers powerful, wise, and practical lessons that teach us to : Develop Joyful Thoughts, Follow Our Life's Mission and Calling, Cultivate Self-Discipline and Act Courageously, Value Time as Our Most Important Commodity, Nourish Our Relationships, and Live Fully, One Day at a Time. ",
+    price: 1260,
+    author: 'Robin Sharma',
+    id: 2
+  }
+]
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Container sx={{ py: 8 }} maxWidth="lg">
+         <Grid container spacing={1}>
+          
+            {
+              dummyData.map(item => <Grid item xs={12} md={4} sm={6}>
+                <Link to={"/book/"+item.id} state={{ item }}>
+                  <ProductCard item={item}/>
+                </Link>
+              </Grid>)
+            }
+            
+            
+          
+        </Grid>
+      </Container>
+     
+    </div>
   )
 }
 
